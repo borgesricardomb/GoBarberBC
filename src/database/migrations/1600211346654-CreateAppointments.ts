1c33bb9e6c89db2c5673c722889f9f7f979e0511
borgesricardomb <borges.ricardomb@gmail.com>
2020-09-17 07:28:48 -0300

    creating in database an appointment

--- src/database/migrations/1600211346654-CreateAppointments.ts
+++ src/database/migrations/1600211346654-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1600211346654
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,20 +1,15 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  //  Um contructor é a funcionalidade de instanciar um objeto daquela classe. Quando fazemos new Date(), por exemplo,
-  //  o constructor passa os parâmetros para a classe e constrói o objeto na memória.
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;

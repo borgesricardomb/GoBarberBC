528ae7ce4630b82b72c3adaafd7bc3514ebd00da
borgesricardomb <borges.ricardomb@gmail.com>
2020-09-20 14:25:41 -0300

    created users

--- src/database/migrations/1600619384600-CreateUsers.ts
+++ src/database/migrations/1600619384600-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1600211346654
-  implements MigrationInterface {
+export default class CreateUsers1600619384600 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +14,13 @@ export default class CreateAppointments1600211346654
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
-            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
           },
           {
             name: 'created_at',
@@ -34,12 +32,16 @@ export default class CreateAppointments1600211346654
             type: 'timestamp',
             default: 'now()',
           },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
         ],
       }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }

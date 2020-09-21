5582d4e1b766812d7ecb7e17dbd97885fdb2874a
borgesricardomb <borges.ricardomb@gmail.com>
2020-09-21 07:01:34 -0300

    start of authentication

---
+++ src/routes/sessionsRoutes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import bodyParser from 'body-parser';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.use(bodyParser.json());
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    await authenticateUser.execute({ email, password });
+
+    return response.json({ ok: true });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,29 @@
+import { compare } from 'bcryptjs';
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<void> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+  }
+}
+
+export default AuthenticateUserService;

1dc5088eae866918da325dc4682f1f9570516777
borgesricardomb <borges.ricardomb@gmail.com>
2020-09-25 07:11:41 -0300

    errors handling

--- src/routes/sessionsRoutes.ts
+++ src/routes/sessionsRoutes.ts
@@ -8,20 +8,16 @@ const sessionsRouter = Router();
 sessionsRouter.use(bodyParser.json());
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
